a3cb1a81da3b8955dba68b6f882059cdbcc051fb
Bartosz Kostrowiecki <bartosz.kostrowiecki@epicode.net>
2018-05-22 18:04:58 +0200

    More logging

--- src/teamcity/TeamcityBuild.ts
+++ src/teamcity/TeamcityBuild.ts
@@ -19,7 +19,7 @@ export class TeamcityBuild implements PivotalTrackerProcessable {
 
     async getTasks(): Promise<StoryHash[]> {
         this.validateTeamcityService();
-        
+
         console.log('Get tasks');
         console.log(this.response.buildId);
         const commitMessages = await this.teamcityService.getCommitMessagesFromBuild(this.response.buildId);
@@ -66,5 +66,6 @@ export class TeamcityBuild implements PivotalTrackerProcessable {
             console.log('No teamcity service available');
             throw 'No teamcity service available';
         }
+        console.log('Teamcity service found');
     }
 }
